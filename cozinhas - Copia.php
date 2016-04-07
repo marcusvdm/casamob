3e26bebf0543c278ceec2a5ab7d71b65be916da6
Marcus Martini <marcusmartini@yahoo.com.br>
2016-04-07 21:38:59 +0000

    updete files

---
+++ cozinhas - Copia.php
@@ -0,0 +1,131 @@
+
+<section class="step produtos" id="produtos" 
+  data-panel="produtos"
+>
+  <div class="page-container">
+    
+    <div class="content linha linha-play js-anchor" data-stellar-offset-parent="true">
+      <div class="offset">
+  
+        <h2 class="cozinhas-title">
+          <small>Linha</small> Play
+        </h2>
+  
+        <div class="modelo play-modelo">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/modelo-play.png" alt="">
+        </div>
+  
+        <div class="cozinha play-cozinha">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/cozinha-play.png" alt="">
+        </div>
+  
+      </div><!-- /.offset -->
+  
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>#play" data-panel="play" class="btn">Clique e conheça</a>
+  
+  
+    </div><!-- /.content.linha-play -->
+  
+    <div class="content linha linha-city js-anchor" data-stellar-offset-parent="true">
+  
+      <div class="offset">
+      
+        <h2 class="cozinhas-title">
+         <h2 class="cozinhas-title" data-stellar-ratio="2"> 
+          <small>Linha</small> City
+        </h2>
+  
+        <div class="modelo city-modelo">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/modelo-city.png" alt="">
+        </div>
+  
+        <div class="cozinha city-cozinha">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/cozinha-city.png" alt="">
+        </div>
+  
+      </div><!-- /.offset -->
+  
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>#city" data-panel="city" class="btn">Clique e conheça</a>
+  
+  
+    </div><!-- /.content.linha-city -->
+  
+    <div class="content linha kits js-anchor" data-stellar-offset-parent="true">
+  
+      <div class="offset">
+  
+        <h2 class="cozinhas-title">
+          <small style="visibility: hidden">Linha</small> Kits
+        </h2>
+  
+        <div class="modelo kits-modelo">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/modelo-kits.png" alt="">
+        </div>
+        
+        <div class="cozinha kits-cozinha">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/cozinha-kits.png" alt="">
+        </div>
+  
+      </div><!-- /.offset -->
+  
+       <a href="<?php echo esc_url( home_url( '/' ) ); ?>#kits" data-panel="kits" class="btn">Clique e conheça</a> 
+      <!--<span class="btn btn--placeholder">Novidades em Breve</span>-->
+  
+  
+    </div><!-- /.content.linha-kits -->
+    
+    <div class="content linha linha-unica js-anchor" data-stellar-offset-parent="true">
+      <div class="offset">
+  
+        <h2 class="cozinhas-title">
+          <small>Linha</small> Unica 3.5
+        </h2>
+  
+        <div class="cozinha play-cozinha">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/cozinha-unica.png" alt="">
+        </div>
+  
+    </div><!-- /.offset -->
+  
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>#unica" data-panel="unica" class="btn">Clique e conheça</a>
+  
+  
+    </div><!-- /.content.linha-unica -->
+    
+    <div class="content linha linha-smart js-anchor" data-stellar-offset-parent="true">
+      <div class="offset">
+  
+        <h2 class="cozinhas-title">
+          <small>Linha</small> Play Smart
+        </h2>
+  
+        <div class="cozinha play-cozinha">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/cozinha-smart.png" alt="">
+        </div>
+  
+    </div><!-- /.offset -->
+  
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>#smart" data-panel="smart" class="btn">Clique e conheça</a>
+  
+  
+    </div><!-- /.content.linha-smart -->
+    
+    <div class="content linha linha-quadri js-anchor" data-stellar-offset-parent="true">
+      <div class="offset">
+  
+        <h2 class="cozinhas-title">
+          <small>Linha</small> Quadri
+        </h2>
+  
+        <div class="cozinha play-cozinha">
+          <img src="<?php bloginfo('template_url');?>/assets/images/cozinhas/cozinha-quadri-2.png" alt="">
+        </div>
+  
+    </div><!-- /.offset -->
+  
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>#quadri" data-panel="quadri" class="btn">Clique e conheça</a>
+  
+  
+    </div><!-- /.content.linha-smart -->
+  </div>
+</section>
